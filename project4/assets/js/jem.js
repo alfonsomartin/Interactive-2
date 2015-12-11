6f17f2cba42315545bbaba3b92a554ccb5238c57
amartin3 <alfonsomartin344@gamil.com>
2015-12-11 13:58:26 -0800

    jem me

--- project4/assets/js/jem.js
+++ project4/assets/js/jem.js
@@ -1,7 +1,14 @@
 // ENTER SITE
 $(document).ready(function(){
-    $(".enter").click(function(){
-        $(".enter").fadeOut()
+    $(".full-screen").click(function(){
+        $(".full-screen").fadeOut()
+    });
+
+});
+// ENTER SITE
+$(document).ready(function(){
+    $(".full-screen").click(function(){
+        $("body").css( "overflow-y" , "scroll" )
     });
 
 });
